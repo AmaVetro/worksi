850fa2313541b4f7dbf90d5ad9672111bda7c976
Vicente Aravena Pavez <ama.vetro28@gmail.com>
2026-07-01 01:16:49 -0400

    Change in Retrofit Android for Deployment

--- producto/frontmovil/app/src/main/java/com/worksi/app/data/api/RetrofitClient.kt
+++ producto/frontmovil/app/src/main/java/com/worksi/app/data/api/RetrofitClient.kt
@@ -9,7 +9,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "https://backend-production-f9dc.up.railway.app/"
 
     val moshi: Moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
